589c0efe337a15e49b142bf79a646bb65bbb97da
Marina Ivanov <marii.ivanov@gmail.com>
2021-08-05 17:04:25 -0300

    Add ginga preparation

---
+++ ginga/src/scriptSemPreparation.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+#for i in {1..10}
+#do
+  ./ginga ../../../Apps/PropagandaDirecionada/mainMan.ncl -d >> saida3.txt 
+	 #hora='date +"%T.%N"' echo $hora >> saida2.txt
+	
+ #sleep 1
+#done
